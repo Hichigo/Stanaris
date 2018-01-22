f0c71f954e7daaa71f8e5d1453e31ab3899bb4db
hichigo <hichigo91@gmail.com>
2018-01-22 13:59:10 +0300

    created stats component and added hero stats struct

---
+++ Source/Stanaris/Classes/Hero/Components/StatsComp.h
@@ -0,0 +1,86 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "StatsComp.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FBar {
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Current;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Max;
+
+	FBar()
+	{
+		Current = 100;
+		Max = 100;
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FHeroStats {
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Level;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FBar Experience;
+		
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FBar Health;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FBar Endurance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Defence;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Damage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 RegenHealthPerSec;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 RegenEndurancePerSec;
+
+	FHeroStats()
+	{
+		Level = 1;
+		Defence = 0;
+		Damage = 3;
+		RegenHealthPerSec = 0;
+		RegenEndurancePerSec = 15;
+	}
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class STANARIS_API UStatsComp : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UStatsComp();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stanaris|Stats")
+	FHeroStats Stats;
+
+};
