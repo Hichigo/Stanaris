691233f6849a3cfe7683af4217473798a32cc8ef
Ivan <hichigo91@gmail.com>
2018-02-12 17:14:29 +0300

    created touch item interface

---
+++ Source/Stanaris/Private/Interfaces/TouchItem.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Interfaces/TouchItem.h"
+
+
+// Add default functionality here for any ITouchItem functions that are not pure virtual.
+UTouchItem::UTouchItem(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
