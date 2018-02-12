37b1e2b06fd5d4ca665f9055378c8f6cef4b875d
Ivan <hichigo91@gmail.com>
2018-02-12 17:22:50 +0300

    renamed touch interface, added use item interface

---
+++ Source/Stanaris/Private/Interfaces/TouchItem_Interface.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Interfaces/TouchItem_Interface.h"
+
+
+// Add default functionality here for any ITouchItem_Interface functions that are not pure virtual.
+UTouchItem_Interface::UTouchItem_Interface(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
---
+++ Source/Stanaris/Private/Interfaces/UseItem_Interface.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Interfaces/UseItem_Interface.h"
+
+
+// Add default functionality here for any IUseItem_Interface functions that are not pure virtual.
+UUseItem_Interface::UUseItem_Interface(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
