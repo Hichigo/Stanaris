ca0bb157a16f17946557078f0764c209af2b2ef3
Ivan <hichigo91@gmail.com>
2018-02-13 17:34:24 +0300

    added base functionality to hero

--- Source/Stanaris/Classes/Hero/HeroPawn.h
+++ Source/Stanaris/Classes/Hero/HeroPawn.h
@@ -4,28 +4,52 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Hero/Components/StatsComp.h"
 #include "HeroPawn.generated.h"
 
-UCLASS()
+UCLASS(config = Game)
 class STANARIS_API AHeroPawn : public ACharacter
 {
 	GENERATED_BODY()
 
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
 public:
 	// Sets default values for this character's properties
-	AHeroPawn();
+	AHeroPawn(const FObjectInitializer& ObjectInitializer);
+
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	/** Called for forwards/backward input */
+	void MoveForward(float Value);
+
+	/** Called for side to side input */
+	void MoveRight(float Value);
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+	/** Returns StatsComp subobject **/
+	FORCEINLINE class UStatsComp* GetStatsComp() const { return StatsComp; }
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UStatsComp* StatsComp;
 	
 };
