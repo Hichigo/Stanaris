417dbbb6a5cf97b533a15268beb8e996fa17a9c0
hichigo <hichigo91@gmail.com>
2018-02-20 15:07:27 +0300

    created player controller cpp

---
+++ Source/Stanaris/Private/Game/StanarisPC.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Game/StanarisPC.h"
+
+AStanarisPC::AStanarisPC(const FObjectInitializer & ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
