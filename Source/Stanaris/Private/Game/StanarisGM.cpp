9c5c17c50a14055a1250dc36e1b3c251788b93fc
hichigo <hichigo91@gmail.com>
2018-02-20 14:57:38 +0300

    created cpp game mode

---
+++ Source/Stanaris/Private/Game/StanarisGM.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Game/StanarisGM.h"
+
+AStanarisGM::AStanarisGM(const FObjectInitializer & ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
