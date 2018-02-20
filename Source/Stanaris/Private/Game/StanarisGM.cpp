75de2f0c1368f8b7350242b3dfeccf9226a8397f
hichigo <hichigo91@gmail.com>
2018-02-20 15:02:50 +0300

    set default HUD

--- Source/Stanaris/Private/Game/StanarisGM.cpp
+++ Source/Stanaris/Private/Game/StanarisGM.cpp
@@ -1,9 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Game/StanarisGM.h"
+#include "Game/StanarisHUD.h"
 
 AStanarisGM::AStanarisGM(const FObjectInitializer & ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-
+	HUDClass = AStanarisHUD::StaticClass();
 }
