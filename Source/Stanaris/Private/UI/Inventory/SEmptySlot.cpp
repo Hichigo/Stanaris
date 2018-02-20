f9963509792d45c9d60bc4a030cac1841630caeb
hichigo <hichigo91@gmail.com>
2018-02-20 16:23:19 +0300

    created empty slot

---
+++ Source/Stanaris/Private/UI/Inventory/SEmptySlot.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UI/Inventory/SEmptySlot.h"
+#include "ConstructorHelpers.h"
+#include "Engine/Texture.h"
+#include "SlateOptMacros.h"
+#include "SBox.h"
+#include "SBorder.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+void SEmptySlot::Construct(const FArguments& InArgs)
+{
+	
+
+
+	const ConstructorHelpers::FObjectFinder<UTexture> MyImage(TEXT("Texture2D'/Game/Stanaris/Hero/UI/Textures/T_EmptySlot.T_EmptySlot'"));
+
+	ChildSlot
+	[
+		// Populate the widget
+		SNew(SBox)
+		.WidthOverride(50)
+		.HeightOverride(50)
+		[
+			SNew(SBorder)
+			//.BorderImage(this, MyImage.Object)
+		]
+	];
+	
+}
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
