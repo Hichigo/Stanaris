c8ac96c23b7e1ece11223a1025f5ea40fef293a2
hichigo <hichigo91@gmail.com>
2018-02-20 16:23:46 +0300

    created empty slot style

---
+++ Source/Stanaris/Classes/UI/Style/EmptySlotWidgetStyle.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Styling/SlateWidgetStyle.h"
+#include "SlateWidgetStyleContainerBase.h"
+
+#include "EmptySlotWidgetStyle.generated.h"
+
+/**
+ * 
+ */
+USTRUCT()
+struct STANARIS_API FEmptySlotStyle : public FSlateWidgetStyle
+{
+	GENERATED_USTRUCT_BODY()
+
+	FEmptySlotStyle();
+	virtual ~FEmptySlotStyle();
+
+	// FSlateWidgetStyle
+	virtual void GetResources(TArray<const FSlateBrush*>& OutBrushes) const override;
+	static const FName TypeName;
+	virtual const FName GetTypeName() const override { return TypeName; };
+	static const FEmptySlotStyle& GetDefault();
+
+	UPROPERTY(EditAnywhere, Category = Appearance)
+	FSlateBrush BackgroundBrush;
+	FEmptySlotStyle& SetBackgroundBrush(const FSlateBrush& InBackgroundBrush) { BackgroundBrush = InBackgroundBrush; return *this; }
+};
+
+/**
+ */
+UCLASS(hidecategories=Object, MinimalAPI)
+class UEmptySlotWidgetStyle : public USlateWidgetStyleContainerBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UEmptySlotWidgetStyle(const FObjectInitializer& ObjectInitializer);
+
+	/** The actual data describing the widget appearance. */
+	UPROPERTY(Category=Appearance, EditAnywhere, meta=(ShowOnlyInnerProperties))
+	FEmptySlotStyle WidgetStyle;
+
+	virtual const struct FSlateWidgetStyle* const GetStyle() const override
+	{
+		return static_cast< const struct FSlateWidgetStyle* >( &WidgetStyle );
+	}
+};
---
+++ Source/Stanaris/Private/UI/Style/EmptySlotWidgetStyle.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UI/Style/EmptySlotWidgetStyle.h"
+
+
+FEmptySlotStyle::FEmptySlotStyle()
+{
+}
+
+FEmptySlotStyle::~FEmptySlotStyle()
+{
+}
+
+const FName FEmptySlotStyle::TypeName(TEXT("FEmptySlotStyle"));
+
+const FEmptySlotStyle& FEmptySlotStyle::GetDefault()
+{
+	static FEmptySlotStyle Default;
+	return Default;
+}
+
+void FEmptySlotStyle::GetResources(TArray<const FSlateBrush*>& OutBrushes) const
+{
+	// Add any brush resources here so that Slate can correctly atlas and reference them
+	OutBrushes.Add(&BackgroundBrush);
+}
+
+UEmptySlotWidgetStyle::UEmptySlotWidgetStyle(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
